4993440e67ce9e62c2b69bc98227642b41fe6a57
Madhuri <madhuri.renduchinthala@gmail.com>
2026-07-25 22:22:09 +0530

    Day 1: variables, types, and string basics

---
+++ Day1.py
@@ -0,0 +1,12 @@
+name = 'Lakshmi Madhuri' #String
+YOE = '8' #Integer
+Age = 33 #Integer
+MamaFeelsGood = True #Boolean
+Exp = "I have 8 years of QA experience"
+WannaBe = 10
+
+print(int(YOE)+5)
+print(Exp.replace('8', str(WannaBe)))
+print(Exp.replace('QA', "Automation QA"))
+#print(int(' '))
+print(type(float('8.5')))
